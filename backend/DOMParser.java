ac2548fdc4eecc4a40a968a51878f9df448eab4f
Oskar-Gudmundur-Kristinsson <ogk2@hi.is>
2021-03-26 01:06:36 +0000

    ^

--- backend/DOMParser.java
+++ backend/DOMParser.java
@@ -10,6 +10,7 @@ public class DOMParser{
     public List<String> sendOrder = new ArrayList<>();
     public String[] sortedData;
     public boolean isSorted = false;
+
     public DOMParser(File f,int mode){ //mode 1 for DOMveiw 2 for change in last traded price
         if(mode == 1){
             sendTradeData(f);
@@ -23,7 +24,9 @@ public class DOMParser{
                 String data = readDOM.nextLine();
                 data = timeValue(data);
                 //System.out.println(data +" std");
-                sendOrder.add(data);
+                if(data != ""){
+                    sendOrder.add(data);
+                }
             }
             readDOM.close();
         }catch (FileNotFoundException e) {
@@ -34,25 +37,33 @@ public class DOMParser{
     }
 
     public String timeValue(String s){
-        String tag = "52=";
-        String ret = s.substring(s.indexOf(tag)+4);
-        int i = 0;
-        //System.out.println(ret+ " tv");
-        while(i < ret.length()){
-            if(Character.getNumericValue(ret.charAt(i)) == -1){
-                //System.out.println(String.valueOf(ret.charAt(i)) + " char at");
-                return (ret.substring(0,i)) + "!t!"+ s;
+        //might as well delete entries we dont need
+        String payloadTag = "269=";
+        int index = s.indexOf(payloadTag);
+        if(index == -1){
+            return "";
+        }
+        else if(Character.getNumericValue(s.charAt(index+4)) < 3){//check if the char after 269 is 0 1 or 2
+            String tag = "52=";
+            String ret = s.substring(s.indexOf(tag)+3);
+            int i = 0;
+            //System.out.println(ret+ " tv");
+            while(i < ret.length()){
+                if(Character.getNumericValue(ret.charAt(i)) == -1){
+                    //System.out.println(String.valueOf(ret.charAt(i)) + " char at");
+                    return (ret.substring(0,i)) + "!t!"+ s;
+                }
+                i++;
             }
-            i++;
         }
         return "";
     }
 
     public void sortData(){
-        String temp = "";
         sortedData = new String [sendOrder.size()];
         sendOrder.toArray(sortedData);
         Arrays.sort(sortedData);
+        isSorted = true;
     }
 
     public static void main(String [] args){
@@ -60,9 +71,6 @@ public class DOMParser{
         DOMParser p =new DOMParser(f,1);
         for(int i= 0; i < 5; i++){
             System.out.println(p.sortedData[i]);
-            System.out.println(" ");
-            System.out.println(" ");
-            System.out.println(" ");
         }
     }
 }
